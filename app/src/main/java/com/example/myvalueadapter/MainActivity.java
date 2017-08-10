be0b82577436a43da2c8a1bbcce3857f354a96e0
chenjing <chenjing>
2017-08-10 14:59:26 +0800

    glide加载图片

--- app/src/main/java/com/example/myvalueadapter/MainActivity.java
+++ app/src/main/java/com/example/myvalueadapter/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 
 import com.example.myvalueadapter.common.AvatarLoadOptions;
 import com.example.myvalueadapter.message.AppConsts;
+import com.example.myvalueadapter.utils.GlideUtis;
 import com.example.myvalueadapter.utils.PowerBarBg;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -15,13 +16,16 @@ public class MainActivity extends AppCompatActivity {
     private String imageUrl2 = "http://thecustomizewindows.com/wp-content/uploads/2011/11/Nicest-Android-Live-Wallpapers.png";
     private ImageView imageView;
     private boolean image = false;
+    private GlideUtis glideUtis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        glideUtis = new GlideUtis(this);
         PowerBarBg.setPowerBarBg(this, getResources().getColor(R.color.background_color));
         imageView = (ImageView) findViewById(R.id.iv);
+        glideUtis.glideCircle(imageUrl, imageView, false);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
